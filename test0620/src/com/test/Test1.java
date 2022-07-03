b6cd0dfdc45a448cdf98668bfda8fde4afd0b5ef
mango <test666@126.com>
2022-07-03 13:33:58 +0800

    提交fastjson测试类

--- test0620/src/com/test/Test1.java
+++ test0620/src/com/test/Test1.java
@@ -1,5 +1,51 @@
 package com.test;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Iterator;
+
 public class Test1 {
-    //3333
+
+
+
+  public static void main(String[] args) {
+    String data = "{\n" +
+            "        userName:\"aa\",\n" +
+            "                age:8,\n" +
+            "            data:[{\n" +
+            "        id:\"99\",\n" +
+            "                pro:\"kk\"\n" +
+            "    },\n" +
+            "        {\n" +
+            "            id:\"99\",\n" +
+            "                    pro:\"kk\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            id:\"99\",\n" +
+            "                    pro:\"kk\"\n" +
+            "        }]\n" +
+            "    }\n";
+    //获取对象
+//    JSONObject obj = JSONObject.parseObject(data);
+//    System.out.println( obj.get("userName"));
+
+    //获取数组
+//    JSONArray jarray = obj.getJSONArray("data");
+//    JSONObject jobj2 = jarray.getJSONObject(0);
+//    System.out.println(jobj2.get("id"));
+
+    //遍历数组
+    JSONObject obj = JSONObject.parseObject(data);
+    JSONArray jarray = obj.getJSONArray("data");
+    Iterator<Object> iterator = jarray.iterator();
+    while (iterator.hasNext()){
+      Object next = iterator.next();
+      JSONObject jsonObject = JSONObject.parseObject(next.toString());
+      System.out.println("id="+ jsonObject.get("id")+"name="+jsonObject.get("pro"));
+    }
+
+
+
+  }
 }
